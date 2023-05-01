3b63a4339260cdbeb028c1ec07089a39c770d96b
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-05-01 11:22:47 +0900

    build: :card_file_box: Add MongoDB
    Add MongoDB as a storage and implemented methods for working with it

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,17 @@
 package user
+
+import (
+	"context"
+
+	"github.com/RAprogramm/rest-api-tutorial/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (u User, err error) {
+	// TODO for next one
+	return
+}
